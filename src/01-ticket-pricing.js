0a931a2d56fbd1065562f9ceb0e7819895c0c836
pradeeppratihast <pradeep.pratihast016@gmail.com>
2026-02-10 01:25:18 +0530

    solved and passed all the test of  01-ticket-pricing.js

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -23,5 +23,27 @@
  * @returns {number} The ticket price, or -1 for invalid input
  */
 export function getTicketPrice(age, isWeekend) {
-  // Your code here
+  let ticketPrice;
+
+  if (!(age<0) && !(Number.isNaN(age))) {
+    if (age>=0 && age<=12) {
+      ticketPrice = 8;
+    } else if (age>=13 && age<=17) {
+      ticketPrice = 12;
+    } else if (age>=18 && age<=59) {
+      ticketPrice = 15;
+    } else if (age>=60) {
+      ticketPrice = 10;
+    } else {
+      ticketPrice = -1;
+    }
+    
+    if (isWeekend) {
+      return ticketPrice + 3;
+    } else {
+      return ticketPrice;
+    }
+  } else {
+    return -1;
+  }
 }
