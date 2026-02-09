12e2f77a42ac84fbefa35dba3560d44fec21b284
pradeeppratihast <pradeep.pratihast016@gmail.com>
2026-02-10 01:44:55 +0530

    solved and passed all the test of  02-traffic-light.js

--- src/02-traffic-light.js
+++ src/02-traffic-light.js
@@ -22,5 +22,23 @@
  * @returns {string} The driving action to take
  */
 export function getTrafficAction(color) {
-  // Your code here
+  const color1 = color.toLowerCase();
+
+  switch (color1) {
+    case "green":
+      return "GO";
+      break;
+    case "yellow":
+      return "SLOW DOWN";
+      break;
+    case "red":
+      return "STOP";
+      break;
+    case "flashing red":
+      return "STOP AND PROCEED WITH CAUTION";
+      break;
+    default:
+      return "INVALID SIGNAL";
+      break;
+  }
 }
